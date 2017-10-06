500710491eda1aedff5243cd6b9b27337ce75b71
Hanif Putra <hanzputro@gmail.com>
2017-10-07 02:22:03 +0700

    margin, section, header

--- gulpfile.js
+++ gulpfile.js
@@ -25,13 +25,12 @@ elixir(function(mix) {
     /* From Plugin */
     mix.styles(
         [
-            'slick-carousel/slick/slick.css'
+            'page-scroll-effects/css/style.css'
         ],'dist/css/plugins.css',bowerDir);
+
     /* core */
-    mix.sass(
-        [
-            'core.scss'
-        ],'dist/css/base.css');
+    mix.sass( [ 'core.scss' ], 'dist/css/base.css' );
+    mix.sass( [ 'pages/home.scss' ], 'dist/css/home.css' );
 
     //####################### JS #######################//
     /* From Plugin */
@@ -39,7 +38,10 @@ elixir(function(mix) {
         [
             // 'jquery/jquery-1.11.3.min.js',       
             'scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
-            'responsive-img.js/responsive-img.js'
+            'responsive-img.js/responsive-img.js',
+            'page-scroll-effects/js/velocity.min.js',
+            'page-scroll-effects/js/velocity.ui.min.js',
+            'page-scroll-effects/js/main.js'
         ], 'dist/js/plugins.js',bowerDir);
 
     /* main */
@@ -54,7 +56,9 @@ elixir(function(mix) {
         "css/plugins.css",
         "css/base.css",
         "js/plugins.js",
-        "js/base.js"
+        "js/base.js",
+
+        "css/home.css"
     ]);
 
     mix.browserSync({
--- home.php
+++ home.php
@@ -10,33 +10,82 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Creative City Worship</title>
-    <meta name="author" content="PT. Qeon Interactive">
-    <meta name="keywords" content="Digital Media, Digital Media Company, Location Based Advertising, Hotspot Advertising, Biznet, Advertisement, period based advertising, performance based advertising">
-    <meta name="description" content="Our Digital Media is the best for Location Based Advertising. We deliver banner and video advertising directly to the device of each user on specific location">
+    <meta name="author" content="Creative City Worship">
+    <meta name="keywords" content="Creative City Worship">
+    <meta name="description" content="Creative City Worship">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon -->
     <!-- <link rel="icon" type="image/png" href="{{URL::asset('/favicon/favicon.png')}}"> -->
     
-    <link rel="stylesheet" href="<?php echo elixir('css/plugins.css'); ?>">
+    <!-- <link rel="stylesheet" href="<?php echo elixir('css/plugins.css'); ?>"> -->
     <link rel="stylesheet" href="<?php echo elixir('css/base.css'); ?>">
+    <link rel="stylesheet" href="<?php echo elixir('css/home.css'); ?>">
 </head>
 
-<body id="body" class="">
-    <div class="container header"></div>    
-    <div class="container" id="section1"></div>
-    <div class="container" id="section2"></div>
-    <div class="container" id="section3"></div>
-    <div class="container" id="section4"></div>
-    <div class="container" id="section5"></div>
+<!-- hijacking: on/off - animation: none/scaleDown/rotate/gallery/catch/opacity/fixed/parallax -->
+<body data-hijacking="on" data-animation="none">
+    <div class="container header">
+        <div class="container1600">
+            <div class="logo"><img src="dist/images/logo-ccw.png" alt=""></div>
+            <div class="navbar">
+                <ul>
+                    <li><a href="">ABOUT CONNECT</a></li>
+                    <li><a href="">CONNECT</a></li>
+                    <li><a href=""><img src="dist/images/logo-ccw.png" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-    <!-- footer -->
-    <div class="containerfull footer">                         
-    </div>   
+    <section class="cd-section visible" id="section1">
+        <div class="section-cover">
+            <div class="section">
+                <h2>Page Scroll Effects</h2>
+                <p>aaaaaaaaa</p>
+            </div>            
+        </div>
+    </section>
 
+    <section class="cd-section" id="section2">
+        <div class="section-cover">
+            <div class="section">
+                <h2>Section 2</h2>
+                <p>bbbbbbbbbb</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="cd-section" id="section3">
+        <div class="section-cover">
+            <div class="section">
+                <h2>Section 3</h2>
+                <p>ccccccccccc</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="cd-section" id="section4">
+        <div class="section-cover">
+            <div class="section">
+                <h2>Section 4</h2>
+                <p>dddddddddddd</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="cd-section" id="section5">
+        <div class="section-cover">
+            <div class="section">
+                <h2>Section 5</h2>
+                <p>eeeeeeeeeeee</p>
+            </div>
+        </div>
+    </section>
 </body>
 
-<!-- <script src="resources/assets/vendor/jquery/jquery-1.11.3.min.js"></script> -->
+<!-- JS -->
+<script src="assets/vendor/jquery/jquery-1.11.3.min.js"></script>
 <script src="<?php echo elixir('js/plugins.js'); ?>"></script>
-<script src="<?php echo elixir('js/base.js'); ?>"></script>
+<!-- <script src="<?php echo elixir('js/base.js'); ?>"></script> -->
 
 </html>
