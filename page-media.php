fd5d466e09edae4acf656067791374ca4bf8e20c
Hanif Putra <hanzputro@gmail.com>
2017-11-30 18:13:10 +0700

    ministry page

---
+++ page-media.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Template Name: The Leaders Page
+*/
+?>
+<?php get_header(); ?>
+
+<!-- specific page -->
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/dist/css/theleaders.css">
+
+
+	<div class="container1600" id="section1">
+		<div class="container__content grid">
+			<div class="span12 section__content">
+	            <nav class="tablink center">
+	                <ul>
+	                    <li class=""><a href="#notset"><h2 class="margin0">Who We Are</h2></a></li>
+	                    <li class=""><a href="#notset"><h2 class="margin0">Service</h2></a></li>
+	                    <li class="active"><a href="#notset"><h2 class="margin0">The Leaders</h2></a></li>
+	                </ul>
+	            </nav>                    
+	        </div>
+		</div>        
+    </div>
+	
+	<div class="container1600" id="section2">
+    	<div class="container__content grid">
+    		<div class="span12 center">
+	            <h1 class="s50 merriweather bold margin0">The Leaders</h1>
+	            <hr class="hr55">
+	            <p class="margin0 s24 myriadpro purple">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum lorem a volutpat euismod.<br>
+				Vivamus blandit metus eu sem blandit, eget dictum neque commodo. Cras fermentum sollicitudin laoreet. Aliquam quis tellus ultricies, iaculis justo ut, vulputate nibh. Fusce convallis odio vitae justo volutpat dictum. Sed non ligula odio. Praesent dapibus lobortis vestibulum. Ut accumsan sem nibh, dapibus mollis nisi ultrices ut.
+				</p>
+	        </div>    		
+    	</div>        
+    </div>
+
+    <div class="container1600" id="section3">
+    	<div class="container__content grid">
+    		<div class="span12">
+	            <h1 class="s50 merriweather bold margin0">Committee</h1>
+				<ul class="clearfix">
+	    			<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+	    		</ul>
+	        </div>    		
+    	</div>        
+    </div>
+	
+	<div class="container1600" id="section4">
+    	<div class="container__content grid">
+    		<div class="span12">
+	            <h1 class="s50 merriweather bold margin0">Head of Departments</h1>
+				<ul class="clearfix">
+	    			<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+	    		</ul>
+	        </div>    		
+    	</div>        
+    </div>
+
+    <div class="footer">
+        <div class="container1600 clearfix">
+            <div class="copyright"><h5 class="margin0 merriweather">Copyright 2017, Creative City Worship, All Right Reserved</h5></div>
+            <ul class="footer-menu">
+                <li><a href=""><h5 class="margin0 merriweather">Creative Community</h5></a></li>
+                <li><a href=""><h5 class="margin0 merriweather">Services</h5></a></li>
+                <li><a href=""><h5 class="margin0 merriweather">Get Connected</h5></a></li>
+            </ul>
+        </div>
+    </div>
+	
+</body>
+
+<!-- JS -->
+<?php wp_footer(); ?>
+
+</html>
+		
---
+++ page-ministry.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Template Name: The Leaders Page
+*/
+?>
+<?php get_header(); ?>
+
+<!-- specific page -->
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/dist/css/theleaders.css">
+	
+	<div class="container1600" id="section1">
+    	<div class="container__content grid">
+    		<div class="span12 center">
+	            <h1 class="s50 merriweather bold margin0">Get Involved</h1>
+	            <hr class="hr55">
+	            <p class="margin0 s24 myriadpro purple">We believe the church is not somewhere you go, it's something you are. We know that God is moving through His church, and we want you to be a part of it. There are several ways you can get involved at Creative City Worship.</p>
+	        </div>    		
+    	</div>        
+    </div>
+
+    <div class="container1600" id="section2">
+    	<div class="container__content grid">
+    		<div class="span12">
+	            <h1 class="s50 merriweather bold margin0">Volunteers</h1>
+				<p>We believe the church is not somewhere you go, it's something you are. We know that God is moving through His church, and we want you to be a part of it. There are several ways you can get involved at Creative City Worship.</p>
+	        </div>    		
+    	</div>        
+    </div>
+	
+	<div class="container1600" id="section3">
+    	<div class="container__content grid">
+    		<div class="span12">
+	            <h1 class="s50 merriweather bold margin0">Head of Departments</h1>
+				<ul class="clearfix">
+	    			<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+					<li class="list float-left">
+	    				<img src="<?php bloginfo('template_url'); ?>/dist/images/leaders.jpg" alt="">
+	    				<h3>Lorem ipsum</h3>
+						<p class="grey">Committee</p>
+					</li>
+	    		</ul>
+	        </div>    		
+    	</div>        
+    </div>
+
+    <div class="footer">
+        <div class="container1600 clearfix">
+            <div class="copyright"><h5 class="margin0 merriweather">Copyright 2017, Creative City Worship, All Right Reserved</h5></div>
+            <ul class="footer-menu">
+                <li><a href=""><h5 class="margin0 merriweather">Creative Community</h5></a></li>
+                <li><a href=""><h5 class="margin0 merriweather">Services</h5></a></li>
+                <li><a href=""><h5 class="margin0 merriweather">Get Connected</h5></a></li>
+            </ul>
+        </div>
+    </div>
+	
+</body>
+
+<!-- JS -->
+<?php wp_footer(); ?>
+
+</html>
+		
