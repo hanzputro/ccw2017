5140741074f7b0e2f66077e1130891fc1d2f76e5
Hanif Putra <hanzputro@gmail.com>
2018-02-14 16:55:47 +0700

    revert to set date time option

--- options.php
+++ options.php
@@ -72,6 +72,9 @@ function optionsframework_options() {
 		'input2' => '',
 		'input3' => '',
 		'date' => '',
+		'dateday' => '',
+		'datemonth' => '',
+		'dateyear' => '',
 		'time' => '',
 		'image' => '' 
 	);
